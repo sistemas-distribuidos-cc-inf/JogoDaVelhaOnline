b0975d205a3e282173eb2a331abf34c3bd943e6a
Sidney François <sidney.fvm@hotmail.com>
2017-12-07 04:06:50 -0200

    Incrementando uma funcao hashCode para auxiliar na busca e insercao de jogadores

--- src/Player.java
+++ src/Player.java
@@ -18,6 +18,13 @@ public class Player implements Serializable {
 		super();
 	}
 	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
--- src/TelaDoJogo.java
+++ src/TelaDoJogo.java
@@ -1,4 +1,19 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.rmi.RemoteException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 
 public class TelaDoJogo {
-
+	
 }
