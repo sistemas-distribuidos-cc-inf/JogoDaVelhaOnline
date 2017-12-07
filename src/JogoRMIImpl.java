d3098bd06bb04a48b6026ca579fa7343304ebd38
Sidney François <sidney.fvm@hotmail.com>
2017-12-07 02:28:31 -0200

    Esboço do servidor do jogo

---
+++ src/JogoRMIImpl.java
@@ -0,0 +1,6 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class JogoRMIImpl extends UnicastRemoteObject implements JogoRMI{
+
+}
---
+++ src/JogoServidor.java
@@ -0,0 +1,17 @@
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+public class JogoServidor {
+	public static void main(String[] args) {
+		
+		try {
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
